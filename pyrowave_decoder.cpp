95231251df84dccda711c3a0ac0af96c5258da53
Hans-Kristian Arntzen <post@themaister.net>
2025-06-09 13:17:13 +0200

    start noodling out the decoder.

--- pyrowave_decoder.cpp
+++ pyrowave_decoder.cpp
@@ -25,11 +25,16 @@ struct DequantizerPushData
 
 struct Decoder::Impl : public WaveletBuffers
 {
-	BufferHandle meta_buffer, payload_data, quant_data;
+	BufferHandle dequant_meta_buffer, payload_data;
 
-	std::vector<BlockMeta> meta_buffer_cpu;
+	struct DequantMeta
+	{
+		uint32_t offset_u32;
+		float q;
+	};
+
+	std::vector<DequantMeta> dequant_meta_buffer_cpu;
 	std::vector<uint32_t> payload_data_cpu;
-	std::vector<float> quant_data_cpu;
 	int decoded_blocks = 0;
 	int total_blocks_in_sequence = 0;
 	uint32_t last_seq = UINT32_MAX;
@@ -81,11 +86,11 @@ void Decoder::Impl::upload_payload(CommandBuffer &cmd)
 
 bool Decoder::Impl::decode_packet(const BitstreamHeader *header)
 {
-	auto &q = quant_data_cpu[header->block_index];
-	if (q == 0.0f)
+	auto &meta = dequant_meta_buffer_cpu[header->block_index];
+	if (meta.q == 0.0f)
 	{
 		decoded_blocks++;
-		q = decode_quant(header->quant_code);
+		meta.q = decode_quant(header->quant_code);
 	}
 	else
 	{
@@ -93,11 +98,9 @@ bool Decoder::Impl::decode_packet(const BitstreamHeader *header)
 		return true;
 	}
 
-	int blocks_16x16 = int(Util::popcount32(header->ballot));
-	auto *control_words = reinterpret_cast<const uint32_t *>(header + 1);
-	auto *payload_words = control_words + blocks_16x16;
+	auto *payload_words = reinterpret_cast<const uint32_t *>(header);
 
-	if (sizeof(*header) / sizeof(uint32_t) + blocks_16x16 > header->payload_words)
+	if (sizeof(*header) / sizeof(uint32_t) > header->payload_words)
 	{
 		LOGE("payload_words is not large enough.\n");
 		return false;
@@ -107,53 +110,9 @@ bool Decoder::Impl::decode_packet(const BitstreamHeader *header)
 	payload_data_cpu.insert(
 			payload_data_cpu.end(),
 			payload_words,
-			payload_words + header->payload_words -
-			blocks_16x16 - sizeof(*header) / sizeof(uint32_t));
-
-	const auto &mapping = block_32x32_to_8x8_mapping[header->block_index];
-	bool invalid_packet = false;
-
-	Util::for_each_bit(header->ballot, [&](unsigned bit) {
-		int x = int(bit & 3);
-		int y = int(bit >> 2);
-
-		if (x < mapping.block_width_8x8 && y < mapping.block_height_8x8)
-		{
-			int block_8x8 = mapping.block_offset_8x8 + mapping.block_stride_8x8 * y + x;
-			meta_buffer_cpu[block_8x8].offset = offset;
-			meta_buffer_cpu[block_8x8].code_word = *control_words;
-
-			auto &mapping_8x8 = block_meta_8x8[block_8x8];
-
-			auto q_bits = (*control_words >> 16) & 0xf;
-			auto lsbs = *control_words & (mapping_8x8.block_mask << 0);
-			auto msbs = *control_words & (mapping_8x8.block_mask << 1);
-			auto msbs_shift = msbs >> 1;
-			auto sign_mask = (msbs_shift | lsbs) | (q_bits ? mapping_8x8.block_mask : 0);
-			msbs |= msbs_shift;
-			auto cost = Util::popcount32(lsbs) +
-			            Util::popcount32(msbs) +
-			            Util::popcount32(sign_mask) +
-			            q_bits * mapping_8x8.in_bounds_subblocks;
-
-			offset += cost;
-			control_words++;
-		}
-		else
-		{
-			LOGE("16x16 block is out of bounds. (%d, %d) >= (%d, %d)\n",
-			     x, y, mapping.block_width_8x8, mapping.block_height_8x8);
-			invalid_packet = true;
-		}
-	});
-
-	if (offset != payload_data_cpu.size())
-	{
-		LOGE("Invalid packet, offset %zu != %zu.\n", offset, payload_data_cpu.size());
-		return false;
-	}
-
-	return !invalid_packet;
+			payload_words + header->payload_words);
+	meta.offset_u32 = offset;
+	return true;
 }
 
 bool Decoder::Impl::push_packet(const void *data_, size_t size)
@@ -278,17 +237,12 @@ void Decoder::Impl::init_block_meta()
 	info.domain = BufferDomain::Device;
 	info.usage = VK_BUFFER_USAGE_STORAGE_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT;
 
-	info.size = block_count_8x8 * sizeof(BlockMeta);
-	meta_buffer = device->create_buffer(info);
-	device->set_name(*meta_buffer, "meta-buffer");
-	meta_buffer_cpu.resize(block_count_8x8);
+	info.size = block_count_32x32 * sizeof(DequantMeta);
+	dequant_meta_buffer = device->create_buffer(info);
+	device->set_name(*dequant_meta_buffer, "meta-buffer");
+	dequant_meta_buffer_cpu.resize(block_count_32x32);
 
 	payload_data_cpu.reserve(1024 * 1024);
-
-	info.size = block_count_32x32 * sizeof(float);
-	quant_data = device->create_buffer(info);
-	device->set_name(*quant_data, "quant-buffer");
-	quant_data_cpu.resize(block_count_32x32);
 }
 
 bool Decoder::Impl::dequant(CommandBuffer &cmd)
@@ -345,9 +299,8 @@ bool Decoder::Impl::dequant(CommandBuffer &cmd)
 				cmd.push_constants(&push, 0, sizeof(push));
 
 				cmd.set_storage_texture(0, 0, *component_layer_views[component][level]);
-				cmd.set_storage_buffer(0, 1, *meta_buffer);
+				cmd.set_storage_buffer(0, 1, *dequant_meta_buffer);
 				cmd.set_storage_buffer(0, 2, *payload_data);
-				cmd.set_storage_buffer(0, 3, *quant_data);
 				cmd.dispatch((push.resolution.x + 15) / 16, (push.resolution.y + 15) / 16, 1);
 			}
 
@@ -468,10 +421,8 @@ bool Decoder::Impl::decode(CommandBuffer &cmd, const ViewBuffers &views)
 	cmd.begin_region("Decode uploads");
 	{
 		upload_payload(cmd);
-		memcpy(cmd.update_buffer(*meta_buffer, 0, meta_buffer_cpu.size() * sizeof(meta_buffer_cpu.front())),
-		       meta_buffer_cpu.data(), meta_buffer_cpu.size() * sizeof(meta_buffer_cpu.front()));
-		cmd.update_buffer_inline(*quant_data, 0, quant_data_cpu.size() * sizeof(quant_data_cpu.front()),
-		                         quant_data_cpu.data());
+		memcpy(cmd.update_buffer(*dequant_meta_buffer, 0, dequant_meta_buffer_cpu.size() * sizeof(dequant_meta_buffer_cpu.front())),
+		       dequant_meta_buffer_cpu.data(), dequant_meta_buffer_cpu.size() * sizeof(dequant_meta_buffer_cpu.front()));
 		cmd.barrier(VK_PIPELINE_STAGE_2_COPY_BIT, VK_ACCESS_2_TRANSFER_WRITE_BIT,
 		            VK_PIPELINE_STAGE_2_COMPUTE_SHADER_BIT, VK_ACCESS_2_SHADER_STORAGE_READ_BIT);
 	}
@@ -491,8 +442,7 @@ bool Decoder::Impl::decode(CommandBuffer &cmd, const ViewBuffers &views)
 
 void Decoder::Impl::clear()
 {
-	std::fill(meta_buffer_cpu.begin(), meta_buffer_cpu.end(), BlockMeta{});
-	std::fill(quant_data_cpu.begin(), quant_data_cpu.end(), 0.0f);
+	std::fill(dequant_meta_buffer_cpu.begin(), dequant_meta_buffer_cpu.end(), DequantMeta{});
 	decoded_blocks = 0;
 	decoded_frame_for_current_sequence = false;
 	total_blocks_in_sequence = block_count_32x32;
