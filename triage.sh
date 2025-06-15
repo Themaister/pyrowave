7634f500f7b4e05b42d9957fe50a8d2c376bf9ad
Hans-Kristian Arntzen <post@themaister.net>
2025-06-16 00:29:31 +0200

    Tweak the triage script a bit more.

--- triage.sh
+++ triage.sh
@@ -10,6 +10,31 @@ if [ -z $input_file ]; then
 fi
 
 mkdir -p /tmp/triage
+rm -f /tmp/triage/*.csv
+
+csv_header="bpp,xpsnr,ssim,ssimulacra2,vmaf,vmafneg"
+echo $csv_header > /tmp/triage/h264_nvenc.csv
+echo $csv_header > /tmp/triage/hevc_nvenc.csv
+echo $csv_header > /tmp/triage/av1_nvenc.csv
+echo $csv_header > /tmp/triage/h264_vaapi.csv
+echo $csv_header > /tmp/triage/hevc_vaapi.csv
+echo $csv_header > /tmp/triage/av1_vaapi.csv
+echo $csv_header > /tmp/triage/pyrowave.csv
+
+append_stats_to_csv()
+{
+	stats=$1
+	csv=$2
+
+	bpp=$(grep "BPP =" $stats | grep -oE '[^ ]+$')
+	xpsnr=$(grep "W-XPSNR:" $stats | grep -oE '[^ ]+$')
+	ssim=$(grep "SSIM Score:" $stats | grep -oE '[^ ]+$')
+	ssimulacra2=$(grep "Average:" $stats | grep -oE '[^ ]+$')
+	vmaf=$(grep "VMAF:" $stats | grep -oE '[^ ]+$')
+	vmafneg=$(grep "VMAF NEG:" $stats | grep -oE '[^ ]+$')
+
+	echo "$bpp,$xpsnr,$ssim,$ssimulacra2,$vmaf,$vmafneg" >> $csv
+}
 
 encode_nvenc()
 {
@@ -22,8 +47,8 @@ encode_nvenc()
 	echo "============="
 	echo "Encoding with NVENC, bitrate ${bit_rate}."
 	ffmpeg -y -i $input_file -b:v $bit_rate -c:v ${codec}_nvenc \
-		-preset p1 -tune ull -g 1 -rc cbr -bufsize $buf_size $encoded_name >/dev/null
-	ffmpeg -y -i $encoded_name $encoded_name.y4m >/dev/null
+		-preset p1 -tune ull -g 1 -rc cbr -bufsize $buf_size $encoded_name >/dev/null 2>/dev/null
+	ffmpeg -y -i $encoded_name $encoded_name.y4m >/dev/null 2>/dev/null
 	psy-ex-scores.py $input_file $encoded_name.y4m -t 16 | ansi2txt > $stats
 	rm $encoded_name.y4m
 	echo "Stats in ${stats}, reference output in ${encoded_name}."
@@ -34,6 +59,8 @@ encode_nvenc()
 	input_size=$(stat -c %s $input_file)
 	bpp=$(echo "scale=3; 12 * $actual_size / $input_size" | bc)
 	echo "BPP = $bpp" >> $stats
+
+	append_stats_to_csv $stats /tmp/triage/${codec}_nvenc.csv
 }
 
 encode_vaapi()
@@ -46,8 +73,9 @@ encode_vaapi()
 	echo "============="
 	echo "Encoding with VAAPI, bitrate ${bit_rate}."
 	ffmpeg -y -vaapi_device /dev/dri/renderD128 -i $input_file -c:v ${codec}_vaapi \
-		-idr_interval 1 -g 1 -rc_mode CBR -b:v $bit_rate -bufsize $buf_size -vf format=nv12,hwupload $encoded_name >/dev/null
-	ffmpeg -y -i $encoded_name $encoded_name.y4m >/dev/null
+		-idr_interval 1 -g 1 -rc_mode CBR -b:v $bit_rate -bufsize $buf_size \
+		-vf format=nv12,hwupload $encoded_name >/dev/null 2>/dev/null
+	ffmpeg -y -i $encoded_name $encoded_name.y4m >/dev/null 2>/dev/null
 	psy-ex-scores.py $input_file $encoded_name.y4m -t 16 | ansi2txt > $stats
 	rm $encoded_name.y4m
 	echo "Stats in ${stats}, reference output in ${encoded_name}."
@@ -58,6 +86,8 @@ encode_vaapi()
 	input_size=$(stat -c %s $input_file)
 	bpp=$(echo "scale=3; 12 * $actual_size / $input_size" | bc)
 	echo "BPP = $bpp" >> $stats
+
+	append_stats_to_csv $stats /tmp/triage/${codec}_vaapi.csv
 }
 
 encode_pyrowave()
@@ -75,6 +105,7 @@ encode_pyrowave()
 
 	bpp=$(echo "scale=3; 8 * $bytes_per_image / (1920 * 1080)" | bc)
 	echo "BPP = $bpp" >> $stats
+	append_stats_to_csv $stats /tmp/triage/pyrowave.csv
 }
 
 encode_nvenc_group()
@@ -94,25 +125,26 @@ encode_vaapi_group()
 encode_accel_group()
 {
 	encode_nvenc_group $1 $2
-	encode_vaapi_group $1 $2
+	# VAAPI seems to not understand what CBR rate control means :') Ignore it.
+	#encode_vaapi_group $1 $2
 }
 
-#encode_pyrowave 50000k $((50000000 / (60 * 8)))
-#encode_pyrowave 75000k $((75000000 / (60 * 8)))
+encode_pyrowave 50000k $((50000000 / (60 * 8)))
+encode_pyrowave 75000k $((75000000 / (60 * 8)))
 #encode_pyrowave 100000k $((100000000 / (60 * 8)))
 #encode_pyrowave 150000k $((150000000 / (60 * 8)))
 #encode_pyrowave 200000k $((200000000 / (60 * 8)))
 #encode_pyrowave 250000k $((250000000 / (60 * 8)))
 #encode_pyrowave 300000k $((300000000 / (60 * 8)))
 #encode_pyrowave 400000k $((400000000 / (60 * 8)))
-encode_pyrowave 500000k $((500000000 / (60 * 8)))
-#encode_accel_group 50000k 1000k
-#encode_accel_group 75000k 1500k
+#encode_pyrowave 500000k $((500000000 / (60 * 8)))
+encode_accel_group 50000k 1000k
+encode_accel_group 75000k 1500k
 #encode_accel_group 100000k 2000k
 #encode_accel_group 150000k 3000k
 #encode_accel_group 200000k 4000k
 #encode_accel_group 250000k 5000k
 #encode_accel_group 300000k 6000k
 #encode_accel_group 400000k 8000k
-encode_accel_group 500000k 10000k
+#encode_accel_group 500000k 10000k
 
