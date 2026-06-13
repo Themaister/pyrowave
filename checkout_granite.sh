500607ff936a7b889af735f34fea94d3f8a86e26
Hans-Kristian Arntzen <post@themaister.net>
2026-06-13 13:44:56 +0200

    Fix some steamrt build issues.

---
+++ checkout_granite.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Only checks out what is necessary to build standalone.
+#
+GRANITE_COMMIT=0363ec26dc
+
+if [ -d Granite ]; then
+	cd Granite
+	git fetch origin
+	git checkout $GRANITE_COMMIT
+else
+	git clone https://github.com/Themaister/Granite
+	cd Granite
+	git checkout $GRANITE_COMMIT
+fi
+
+cd ..
+
+update() {
+	git submodule sync $1
+	git submodule update --init $1
+}
+
+cd Granite
+update third_party/volk
+update third_party/khronos/vulkan-headers
