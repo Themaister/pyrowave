a4c3e2561d8b154109389a19c18995c4a32dba1a
Hans-Kristian Arntzen <post@themaister.net>
2026-07-04 12:06:00 +0200

    Add decoding to bench tool as well.

--- bench.cpp
+++ bench.cpp
@@ -7,11 +7,54 @@
 #include "device.hpp"
 #include "context.hpp"
 #include "pyrowave_encoder.hpp"
+#include "pyrowave_decoder.hpp"
 #include "yuv4mpeg.hpp"
 #include "shaders/slangmosh.hpp"
 
 using namespace Granite;
 using namespace Vulkan;
+
+static std::vector<uint8_t> example_payload;
+
+static void run_decoder_test(Device &device, PyroWave::Decoder &dec, const PyroWave::ViewBuffers &output)
+{
+	for (uint32_t i = 0; i < 10000; i++)
+	{
+		dec.clear();
+		dec.push_packet(example_payload.data(), example_payload.size());
+		if (!dec.decode_is_ready(false))
+			return;
+
+		auto cmd = device.request_command_buffer();
+
+		cmd->begin_barrier_batch();
+		for (auto &plane: output.planes)
+		{
+			if (PyroWave::Decoder::device_prefers_fragment_path(device))
+			{
+				cmd->image_barrier(plane->get_image(), VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_ATTACHMENT_OPTIMAL,
+				                   VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT, 0,
+				                   VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT,
+				                   VK_ACCESS_2_COLOR_ATTACHMENT_WRITE_BIT);
+			}
+			else
+			{
+				cmd->image_barrier(plane->get_image(), VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_GENERAL,
+				                   VK_PIPELINE_STAGE_COMPUTE_SHADER_BIT, 0,
+				                   VK_PIPELINE_STAGE_COMPUTE_SHADER_BIT, VK_ACCESS_2_SHADER_STORAGE_WRITE_BIT);
+			}
+		}
+		cmd->end_barrier_batch();
+
+		auto start_ts = cmd->write_timestamp(VK_PIPELINE_STAGE_ALL_COMMANDS_BIT);
+		dec.decode(*cmd, output);
+		auto end_ts = cmd->write_timestamp(VK_PIPELINE_STAGE_ALL_COMMANDS_BIT);
+		device.register_time_interval("GPU", std::move(start_ts), std::move(end_ts), "Overall Decode");
+		device.submit(cmd);
+		device.next_frame_context();
+		//LOGI("Submitted decoder frame %05u ...\n", i);
+	}
+}
 
 static void run_encoder_test(Device &device,
                              PyroWave::Encoder &enc,
@@ -41,11 +84,15 @@ static void run_encoder_test(Device &device,
 	buffers.bitstream.size = bitstream->get_create_info().size;
 	buffers.target_size = bitstream_size;
 
+	Fence fence;
+
 	for (uint32_t i = 0; i < 10000; i++)
 	{
 		auto cmd = device.request_command_buffer(CommandBuffer::Type::AsyncCompute);
 		auto start_ts = cmd->write_timestamp(VK_PIPELINE_STAGE_ALL_COMMANDS_BIT);
 		enc.encode(*cmd, inputs, buffers);
+		cmd->barrier(VK_PIPELINE_STAGE_COMPUTE_SHADER_BIT, VK_ACCESS_2_SHADER_STORAGE_WRITE_BIT,
+					 VK_PIPELINE_STAGE_2_COPY_BIT, VK_ACCESS_2_TRANSFER_READ_BIT);
 		auto end_ts = cmd->write_timestamp(VK_PIPELINE_STAGE_ALL_COMMANDS_BIT);
 		device.register_time_interval("GPU", std::move(start_ts), std::move(end_ts), "Overall Encode");
 		start_ts = cmd->write_timestamp(VK_PIPELINE_STAGE_ALL_COMMANDS_BIT);
@@ -56,10 +103,23 @@ static void run_encoder_test(Device &device,
 					 VK_ACCESS_HOST_READ_BIT);
 		end_ts = cmd->write_timestamp(VK_PIPELINE_STAGE_ALL_COMMANDS_BIT);
 		device.register_time_interval("GPU", std::move(start_ts), std::move(end_ts), "Bitstream Readback");
-		device.submit(cmd);
+		fence.reset();
+		device.submit(cmd, &fence);
 		device.next_frame_context();
-		LOGI("Submitted frame %05u ...\n", i);
+		//LOGI("Submitted encoder frame %05u ...\n", i);
 	}
+
+	fence->wait();
+
+	PyroWave::Encoder::Packet packet = {};
+	example_payload.resize(500000);
+	auto num_packets = enc.compute_num_packets(device.map_host_buffer(*meta_host, MEMORY_ACCESS_READ_BIT), 500000);
+	(void)num_packets;
+	assert(num_packets == 1);
+	enc.packetize(&packet, 500000, example_payload.data(), example_payload.size(),
+	              device.map_host_buffer(*meta_host, MEMORY_ACCESS_READ_BIT),
+	              device.map_host_buffer(*bitstream_host, MEMORY_ACCESS_READ_BIT));
+	example_payload.resize(packet.size);
 }
 
 struct YCbCrImages
@@ -73,7 +133,7 @@ static YCbCrImages create_ycbcr_images(Device &device, int width, int height, Vk
 	YCbCrImages images;
 	auto info = ImageCreateInfo::immutable_2d_image(width, height, fmt);
 	info.usage = VK_IMAGE_USAGE_TRANSFER_SRC_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT |
-	             VK_IMAGE_USAGE_STORAGE_BIT | VK_IMAGE_USAGE_SAMPLED_BIT;
+	             VK_IMAGE_USAGE_STORAGE_BIT | VK_IMAGE_USAGE_SAMPLED_BIT | VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
 	info.initial_layout = VK_IMAGE_LAYOUT_UNDEFINED;
 
 	images.images[0] = device.create_image(info);
@@ -112,7 +172,11 @@ static void run_vulkan_test(Device &device, const char *in_path)
 	auto inputs = create_ycbcr_images(device, width, height, fmt, chroma);
 
 	PyroWave::Encoder enc;
+	PyroWave::Decoder dec;
 	if (!enc.init(&device, width, height, chroma))
+		return;
+
+	if (!dec.init(&device, width, height, chroma, PyroWave::Decoder::device_prefers_fragment_path(device)))
 		return;
 
 	if (!input.begin_frame())
@@ -140,7 +204,7 @@ static void run_vulkan_test(Device &device, const char *in_path)
 
 	for (int i = 0; i < 3; i++)
 	{
-		cmd->image_barrier(*inputs.images[i], VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL,
+		cmd->image_barrier(*inputs.images[i], VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, VK_IMAGE_LAYOUT_READ_ONLY_OPTIMAL,
 		                   VK_PIPELINE_STAGE_2_COPY_BIT, VK_ACCESS_2_TRANSFER_WRITE_BIT,
 		                   VK_PIPELINE_STAGE_2_COMPUTE_SHADER_BIT, VK_ACCESS_2_SHADER_SAMPLED_READ_BIT);
 	}
@@ -148,6 +212,22 @@ static void run_vulkan_test(Device &device, const char *in_path)
 	device.submit(cmd);
 
 	run_encoder_test(device, enc, inputs.views);
+	for (int i = 0; i < 4; i++)
+		device.next_frame_context();
+	device.timestamp_log([](const std::string &tag, const TimestampIntervalReport &report)
+	{
+		LOGI("%s -> %.3f us avg\n", tag.c_str(), report.time_per_frame_context * 1e6);
+	});
+	device.timestamp_log_reset();
+
+	run_decoder_test(device, dec, inputs.views);
+	for (int i = 0; i < 4; i++)
+		device.next_frame_context();
+	device.timestamp_log([](const std::string &tag, const TimestampIntervalReport &report)
+	{
+		LOGI("%s -> %.3f us avg\n", tag.c_str(), report.time_per_frame_context * 1e6);
+	});
+	device.timestamp_log_reset();
 }
 
 static void run_vulkan_test(const char *in_path)
