503c42d17d7b1372e3d63afe09fa2d0214ca8aae
Hans-Kristian Arntzen <post@themaister.net>
2026-07-30 17:57:09 +0200

    Fix steamrt build for evaluator.

--- build-steamrt-helper.sh
+++ build-steamrt-helper.sh
@@ -3,15 +3,19 @@
 # This is run from inside the container.
 # Useful as an initial template.
 #
-ls -l
-
 mkdir -p build-steamrt
 cd build-steamrt
+
+# Can omit PYROWAVE_UTILS if just building the .so.
+# Hack for now, copy over the ffmpeg build folder from pyrofling.
+# Symlink won't work due to docker shenanigans.
 
 cmake .. \
 	-DCMAKE_BUILD_TYPE=Release \
 	-DCMAKE_INSTALL_PREFIX=../steamrt-output \
 	-DPYTHON_EXECUTABLE=$(which python3) \
+	-DPYROWAVE_UTILS=ON \
+	-DCMAKE_PREFIX_PATH=/pyro/ffmpeg-build-linux-steamrt/output \
 	-G Ninja
 
 ninja install/strip -v
