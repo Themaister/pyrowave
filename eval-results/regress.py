6b29636a4eb60ffd6757b8fdb7028ba52e6753c0
Hans-Kristian Arntzen <post@themaister.net>
2026-08-11 23:23:20 +0200

    Generate a C header impl.

--- eval-results/regress.py
+++ eval-results/regress.py
@@ -45,6 +45,76 @@ def estimate_psnr(kb, mpixels, chroma444, h, coefficients):
     estimate += chroma444 * kb * mpixels * h * coefficients[27]
     return estimate
 
+class BucketPixelsToSize:
+    def __init__(self, h, c444, psnr):
+        self.h = h
+        self.c444 = c444
+        self.psnr = psnr
+        self.results = []
+
+    def add_pix_to_size_result(self, pix, size):
+        self.results.append((pix, size))
+
+    def finish(self):
+        self.results.sort(key = lambda a: a[0])
+
+# Maps out a curve for size -> psnr
+class BucketSizeToPSNR:
+    def __init__(self, h, pix, c444):
+        self.h = h
+        self.pix = pix
+        self.c444 = c444
+        self.results = []
+
+    def add_size_to_psnr_result(self, size, psnr):
+        self.results.append((size, psnr))
+
+    def finish(self):
+        self.results.sort(key = lambda a: a[1])
+
+    def get_size_requirement_for_psnr(self, psnr):
+        for i in range(len(self.results) - 1):
+            first = self.results[i + 0]
+            second = self.results[i + 1]
+            if psnr >= first[1] and psnr <= second[1]:
+                l = (psnr - first[1]) / (second[1] - first[1])
+                return first[0] + l * (second[0] - first[0])
+        return 0
+
+def find_or_create_bucket_size_to_psnr(buckets, h, pix, c444):
+    for bucket in buckets:
+        if bucket.h == h and bucket.pix == pix and bucket.c444 == c444:
+            return bucket
+    bucket = BucketSizeToPSNR(h, pix, c444)
+    buckets.append(bucket)
+    return bucket
+
+def find_or_create_bucket_pix_to_size(buckets, h, c444, psnr):
+    for bucket in buckets:
+        if bucket.h == h and bucket.c444 == c444 and bucket.psnr == psnr:
+            return bucket
+    bucket = BucketPixelsToSize(h, c444, psnr)
+    buckets.append(bucket)
+    return bucket
+
+def regress(values):
+    pix = [x[0] for x in values]
+    sizes = np.array([x[1] for x in values])
+
+    pix = np.array(pix)
+    pix = np.sqrt(pix / 1e6) - 2
+    pix2 = pix**2
+    pix3 = pix**3
+    pix4 = pix**4
+    pix5 = pix**5
+    pix6 = pix**6
+    pix7 = pix**7
+
+    A = np.column_stack([np.ones_like(pix), pix, pix2, pix3, pix4, pix5, pix6, pix7])
+    coefficients, residuals, _, _ = np.linalg.lstsq(A, sizes, rcond = None)
+    residual_manual = math.sqrt(residuals[0] / len(pix))
+    return coefficients, residual_manual
+
 def main():
     parser = argparse.ArgumentParser(description = 'Script for generating a PSNR estimator JSON files.')
     parser.add_argument('--csv', type = str, action = 'append', help = 'Path to result CSV')
@@ -53,96 +123,128 @@ def main():
     if not args.csv:
         raise ArgumentError('Need --csv')
 
-    kbits = []
-    mpixels = []
-    chroma444 = []
-    h = []
-    psnr = []
+    buckets_size_to_psnr = []
+    buckets_pix_to_size = []
 
     for csv_path in args.csv:
         with open(csv_path, 'r') as f:
             reader = csv.reader(f)
             _ = next(reader)
             for row in reader:
+                kb = float(row[0])
+                pix = float(row[1]) * float(row[2])
+                c444 = float(row[3])
                 for i in range(16):
-                    # Ignore values that are way outside the usable range
-                    if float(row[4 + i]) < 30 or float(row[4 + i]) > 50:
-                        continue
+                    #h = 1 + i / 8
+                    h = i
+                    bucket = find_or_create_bucket_size_to_psnr(buckets_size_to_psnr, h, pix, c444)
+                    psnr = float(row[4 + i])
+                    bucket.add_size_to_psnr_result(kb, psnr)
 
-                    kbits.append(float(row[0]))
-                    mpixels.append(float(row[1]) * float(row[2]) / 1000000)
-                    chroma444.append(float(row[3]))
-                    h.append(1 + i / 8)
-                    psnr.append(float(row[4 + i]))
+    for size_to_psnr_bucket in buckets_size_to_psnr:
+        size_to_psnr_bucket.finish()
 
-    kbits = np.array(kbits)
-    mpixels = np.array(mpixels)
-    chroma444 = np.array(chroma444)
-    h = np.array(h)
-    psnr = np.array(psnr)
+        for psnr in range(30, 51):
+            size_req = size_to_psnr_bucket.get_size_requirement_for_psnr(psnr)
+            if size_req > 0:
+                pix_to_size_bucket = find_or_create_bucket_pix_to_size(buckets_pix_to_size, size_to_psnr_bucket.h, size_to_psnr_bucket.c444, psnr)
+                pix_to_size_bucket.add_pix_to_size_result(size_to_psnr_bucket.pix, size_req)
 
-    # Estimate PSNR as a linear set of equations.
-    # More parameters seem to yield better results somehow.
-    # Constant bias, various moments of the inputs, and cross-moments to extract correlation.
-    A = np.column_stack([
-        np.ones_like(psnr),
-        kbits,
-        kbits**0.5,
-        kbits**2,
-        kbits**1.5,
-        mpixels,
-        mpixels**0.5,
-        mpixels**0.75,
-        mpixels**0.25,
-        h,
-        h**0.5,
-        h**2,
-        h**1.5,
-        kbits * mpixels,
-        kbits * h,
-        mpixels * h,
-        chroma444,
-        chroma444 * kbits,
-        chroma444 * mpixels,
-        chroma444 * h,
-        chroma444 * kbits * kbits,
-        chroma444 * mpixels**0.5,
-        chroma444 * h * h,
-        chroma444 * kbits * mpixels,
-        chroma444 * kbits * h,
-        chroma444 * mpixels * h,
-        kbits * mpixels * h,
-        chroma444 * kbits * mpixels * h])
+    lo_stddev = 1e30
+    hi_stddev = 0
 
-    coefficients, _, _, _ = np.linalg.lstsq(A, psnr, rcond = None)
-    print(coefficients)
+    for bucket in buckets_pix_to_size:
+        bucket.finish()
+        #print(f'Regressing for h = {bucket.h}, c444 = {bucket.c444}, psnr = {bucket.psnr}')
+        coefficients, stddev = regress(bucket.results)
+        #print(f'PSNR = {bucket.psnr} -> stddev {stddev}')
 
-    print(f'PSNR = {coefficients[0]} +')
-    print(f'\t{coefficients[1]} * kB + {coefficients[2]} * sqrt(kB) + {coefficients[3]} * kB^2 + {coefficients[4]} * kB^1.5 +')
-    print(f'\t{coefficients[5]} * MPixels + {coefficients[6]} * sqrt(MPixels) + {coefficients[7]} * MPixels^2 + {coefficients[8]} * MPixels^0.25 +')
-    print(f'\t{coefficients[9]} * H + {coefficients[10]} * sqrt(H) + {coefficients[11]} * H^2 + {coefficients[12]} * H^1.5')
-    print(f'\t{coefficients[13]} * kB * MPixels + {coefficients[14]} * kB * H + {coefficients[15]} * MPixels * H')
+        size = bucket.results[len(bucket.results) // 2][1]
+        stddev /= size
 
-    total_sqr_errors = 0.0
-    for i in range(len(psnr)):
-        estimate = estimate_psnr(kbits[i], mpixels[i], chroma444[i], h[i], coefficients)
-        total_sqr_errors += (estimate - psnr[i]) * (estimate - psnr[i])
+        if stddev > hi_stddev:
+            print(f'New record {stddev} for PSNR {bucket.psnr}, h = {bucket.h}, size = {size}')
+        hi_stddev = max(hi_stddev, stddev)
+        lo_stddev = min(lo_stddev, stddev)
 
-    print('StdDev:', math.sqrt(total_sqr_errors / len(psnr)))
+    print(f'LO {lo_stddev}, HI {hi_stddev}\n\n')
 
-    b = 260.4
-    print(estimate_psnr(b, 1280 * 720 * 1e-6, 0, 1.5, coefficients))
-    print(estimate_psnr(b * math.pow((1920 * 1080) / (1280 * 720), 0.5), 1920 * 1080 * 1e-6, 0, 1.5, coefficients))
-    print(estimate_psnr(b * math.pow((2560 * 1440) / (1280 * 720), 0.5), 2560 * 1440 * 1e-6, 0, 1.5, coefficients))
-    print(estimate_psnr(b * math.pow((3840 * 2160) / (1280 * 720), 0.5), 3840 * 2160 * 1e-6, 0, 1.5, coefficients))
+    print('#ifndef PYROWAVE_REGRESSION_RESULTS_H_')
+    print('#define PYROWAVE_REGRESSION_RESULTS_H_')
+    print('#include <math.h>')
+    print('#include <assert.h>')
+    print('#include <stddef.h>')
+    print('/* Autogenerated by regress.py */')
+    print('enum pyrowave_height_factor {')
+    print('\tPYROWAVE_HEIGHT_FACTOR_1_00 = 0,')
+    print('\tPYROWAVE_HEIGHT_FACTOR_1_12 = 1,')
+    print('\tPYROWAVE_HEIGHT_FACTOR_1_25 = 2,')
+    print('\tPYROWAVE_HEIGHT_FACTOR_1_37 = 3,')
+    print('\tPYROWAVE_HEIGHT_FACTOR_1_50 = 4,')
+    print('\tPYROWAVE_HEIGHT_FACTOR_1_62 = 5,')
+    print('\tPYROWAVE_HEIGHT_FACTOR_1_75 = 6,')
+    print('\tPYROWAVE_HEIGHT_FACTOR_1_87 = 7,')
+    print('\tPYROWAVE_HEIGHT_FACTOR_2_00 = 8,')
+    print('\tPYROWAVE_HEIGHT_FACTOR_2_12 = 9,')
+    print('\tPYROWAVE_HEIGHT_FACTOR_2_25 = 10,')
+    print('\tPYROWAVE_HEIGHT_FACTOR_2_37 = 11,')
+    print('\tPYROWAVE_HEIGHT_FACTOR_2_50 = 12,')
+    print('\tPYROWAVE_HEIGHT_FACTOR_2_62 = 13,')
+    print('\tPYROWAVE_HEIGHT_FACTOR_2_75 = 14,')
+    print('\tPYROWAVE_HEIGHT_FACTOR_2_87 = 15')
+    print('};')
+    print('#define PYROWAVE_REGRESSION_MIN_PSNR_HVS_M_H 30')
+    print('#define PYROWAVE_REGRESSION_MAX_PSNR_HVS_M_H 50')
+    print('#define PYROWAVE_REGRESSION_MIN_PIXELS (1280 * 720)')
+    print('#define PYROWAVE_REGRESSION_MAX_PIXELS (3840 * 2160)')
+    print('static const struct pyrowave_rate_regression_result {')
+    print('\tint psnr_hvs_m_h;')
+    print('\tint height_factor;')
+    print('\tint chroma444;')
+    print('\tdouble poly_coefficients[8];')
+    print('} pyrowave_buckets[] = {')
 
-    for i in range(50):
-        mbits = 100 + i * 10
-        kb = 1000 * mbits / (8 * 60)
-        #print(f'4:2:0 1080p mbits {mbits}, psnr = {estimate_psnr(kb, 1920 * 1080 * 1e-6, 0, 2, coefficients):.4}')
-        #print(f' 4:4:4 1080p mbits {mbits}, psnr = {estimate_psnr(kb, 1920 * 1080 * 1e-6, 1, 2, coefficients):.4}')
-        print(f'  4:2:0 1440p mbits {mbits}, psnr = {estimate_psnr(kb, 2560 * 1440 * 1e-6, 0, 2, coefficients):.4}')
-        print(f'   4:4:4 1440p mbits {mbits}, psnr = {estimate_psnr(kb, 2560 * 1440 * 1e-6, 1, 2, coefficients):.4}')
+    for bucket in buckets_pix_to_size:
+        coefficients, _ = regress(bucket.results)
+        coeffs_str = [str(x) for x in coefficients]
+        coeffs_str = ', '.join(coeffs_str)
+        print('\t{', f'{int(bucket.psnr)}, {int(bucket.h)}, {int(bucket.c444)},', '{', coeffs_str, '}', '},')
+    print('};')
+    print('''
+static double pyrowave_psnr_hvs_m_h_estimate_mbits(
+        int psnr, int width, int height,
+        enum pyrowave_height_factor height_factor, int chroma444, double fps)
+{
+    int num_pixels = width * height;
+    size_t i;
+    assert(psnr >= PYROWAVE_REGRESSION_MIN_PSNR_HVS_M_H && psnr <= PYROWAVE_REGRESSION_MAX_PSNR_HVS_M_H);
+    assert(num_pixels >= PYROWAVE_REGRESSION_MIN_PIXELS && num_pixels <= PYROWAVE_REGRESSION_MAX_PIXELS);
+    
+    for (i = 0; i < sizeof(pyrowave_buckets) / sizeof(pyrowave_buckets[0]); i++)
+    {
+        const struct pyrowave_rate_regression_result *result = &pyrowave_buckets[i];
+        if (result->psnr_hvs_m_h == psnr && result->height_factor == height_factor && result->chroma444 == chroma444)
+        {
+            double pixel_non_linear = (double)num_pixels;
+            double power_chain = 1.0;
+            double estimate = 0.0;
+            int coeff;
+            
+            /* Non-linear warp to make the polynomial estimate more accurate. */
+            pixel_non_linear = sqrt(pixel_non_linear * 1e-6) - 2.0;
+            for (coeff = 0; coeff < 8; coeff++)
+            {
+                estimate += power_chain * result->poly_coefficients[coeff];
+                power_chain *= pixel_non_linear;
+            }
+            
+            return estimate * 8e-3 * fps;
+        }
+    }
+    
+    return 0.0;
+}''')
+    print('#endif /* PYROWAVE_REGRESSION_RESULTS_H_ */')
 
 if __name__ == '__main__':
     main()
